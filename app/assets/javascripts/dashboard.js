3a53a41c1d5ea99de9af347bf0c09ac240b2728e
Luke F Wilson <lfwilson@stanford.edu>
2015-07-06 23:59:30 -0700

    add basic hash routing with routie.js

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,7 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+var connectionsId = '#nav-connections';
+var groupsId = '#nav-groups';
+var emailsId = '#nav-emails';
 
+var selectNavButton = function(navButtonId) {
+  $(connectionsId).removeClass('selected');
+  $(groupsId).removeClass('selected');
+  $(emailsId).removeClass('selected');
+  $(navButtonId).addClass('selected');
+}
 
 var ready = function() {
 
@@ -10,6 +19,43 @@ var ready = function() {
     $("#wrapper").toggleClass("toggled");
   });
 
+  routie({
+    'connections/:id?': function(id) {
+      selectNavButton(connectionsId);
+      if (!id) {
+        console.log("Connections");
+      } else if (id === "new") {
+        console.log("YO lets make a new connection!");
+      } else {
+        console.log("YO!" + id);
+        // check if id exists, else go to connections index
+      }
+    },
+    'groups': function() {
+      selectNavButton(groupsId);
+    },
+    'emails': function() {
+      selectNavButton(emailsId);
+    },
+    '*': function() {
+      selectNavButton(connectionsId);
+    }
+  });
+
+  $(connectionsId).click(function(e) {
+    e.preventDefault();
+    routie('connections');
+  });
+
+  $(groupsId).click(function(e) {
+    e.preventDefault();
+    routie('groups');
+  });
+
+  $(emailsId).click(function(e) {
+    e.preventDefault();
+    routie('emails');
+  });
 };
 
 $(document).ready(ready);
